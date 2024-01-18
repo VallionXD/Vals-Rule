bcf6e1bcc27bbf3f6e4adf3fc2b292471ded8085
Vallion <151787330+VallionXD@users.noreply.github.com>
2024-01-18 16:26:32 -0500

    Website 1.0

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function encode() {
+  const eI = document.getElementById("encodeInput").value;
+  const p = document.getElementById("pattern").value;
+  let e = encodeValsRule(p, eI);
+
+  document.getElementById("encodeOutput").value = e;
+}
+
+function encodeClear() {
+  const eI = document.getElementById("encodeInput");
+  const eO = document.getElementById("encodeOutput");
+
+  // Set to blank
+  eI.value = "";
+  eI.textContent = "";
+  eO.value = "";
+  eO.textContent = "";
+}
+
+function decode() {
+  const dI = document.getElementById("decodeInput").value;
+  const p = document.getElementById("pattern").value;
+  let d = decodeValsRule(p, dI);
+
+  document.getElementById("decodeOutput").value = d;
+}
+
+function decodeClear() {
+  const dI = document.getElementById("decodeInput").value;
+  const dO = document.getElementById("decodeOutput").value;
+
+  // Set to blank
+  dI.value = "";
+  dI.textContent = "";
+  dO.value = "";
+  dO.textContent = "";
+}
